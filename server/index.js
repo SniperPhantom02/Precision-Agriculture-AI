3cbbb070da9b5ace7621f48da61be0f726069248
Ashutosh <sharmaashutosh2610@gmail.com>
2026-04-18 17:47:30 +0000

    Changed the code of the file

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const axios = require("axios");
-const { predictIrrigation } = require("./predict");
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+
+const { predictIrrigation } = require("./predict");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
-// 🔥 ADD THIS BLOCK HERE
+
 async function getWeather(city) {
     const API_KEY = "5753c8287637c30fe4f8f73c77ad9db9";
 
@@ -24,7 +25,43 @@ async function getWeather(city) {
 }
 
 
-// existing route
+
 app.get("/", (req, res) => {
     res.json({ message: "Precision Agriculture API running" });
+});
+
+app.post("/predict", async (req, res) => {
+    try {
+        const { city, soilType, cropType } = req.body;
+
+        if (!city || !soilType || !cropType) {
+            return res.status(400).json({ error: "Missing fields" });
+        }
+
+        const weather = await getWeather(city);
+
+
+        const result = predictIrrigation(
+            weather.temp,
+            weather.rainfall,
+            soilType,
+            cropType
+        );
+
+
+        res.json({
+            ...result,
+            live_weather: weather
+        });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Weather fetch failed" });
+    }
+});
+
+
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
 });

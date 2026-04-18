a42dc2049b7d23f4bb76696a0b6617a0d73a8e7f
Ashutosh <sharmaashutosh2610@gmail.com>
2026-04-18 16:55:07 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Precision Agriculture API running" });
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});

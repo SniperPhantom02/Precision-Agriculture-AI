4bcf51b24a12579a710423449bd8a36e1c78d0f3
Ashutosh <sharmaashutosh2610@gmail.com>
2026-04-18 16:56:51 +0000

    Update predict.js

--- server/predict.js
+++ server/predict.js
@@ -1,1 +1,36 @@
+function predictIrrigation(temp, rainfall, soilType, cropType) {
+    let waterNeed = 0;
 
+    if (temp > 30) waterNeed += 30;
+    else if (temp > 20) waterNeed += 20;
+    else waterNeed += 10;
+
+    if (rainfall > 50) waterNeed -= 15;
+    else if (rainfall > 20) waterNeed -= 5;
+
+    if (soilType === "sandy") waterNeed += 10;
+    else if (soilType === "clay") waterNeed -= 5;
+
+    let risk = "Low";
+    if (temp > 35 && rainfall < 10) risk = "High";
+    else if (temp > 30) risk = "Medium";
+
+    const climateImpact = getClimateImpact(temp, rainfall);
+
+    return {
+        water_liters_per_day: Math.max(waterNeed, 5),
+        risk_level: risk,
+        climate_impact: climateImpact,
+    };
+}
+
+function getClimateImpact(temp, rainfall) {
+    if (temp > 35 && rainfall < 20)
+        return "High climate stress: drought conditions likely";
+    else if (rainfall > 80)
+        return "Excess rainfall: flooding risk";
+    else
+        return "Stable climate conditions";
+}
+
+module.exports = { predictIrrigation };
